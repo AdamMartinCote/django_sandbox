178718b302970fcf4b77a05b70805f50fd25b5db
Adam <amcote@gosecure.net>
2020-04-11 16:23:34 -0400

    feat: part 5 done

--- polls/views.py
+++ polls/views.py
@@ -7,27 +7,63 @@ such as Http404. The rest is up to you.
 
 All Django wants is that HttpResponse. Or an exception.
 """
-from django.http import HttpResponse, Http404
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
-from polls.models import Question
-
-
-def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_questions': latest_questions}
-    return render(request, 'polls/index.html', context)
+from polls.models import Question, Choice
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """Excludes any questions that aren't published yet.
+
+        Returns:
+            QuerySet of published questions
+
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST is a dictionary-like object that lets you access submitted data by key name
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # Use F() to avoid race condition if 2 users interlace their GET and POST
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # HttpResponseRedirect takes a single argument: the URL to which the user will be redirected
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
